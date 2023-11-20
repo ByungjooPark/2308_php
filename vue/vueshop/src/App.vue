8d9f65124a364d9eae41f4131454d6a68f41e830
greenmeerkat <milkpbj23@gmail.com>
2023-11-20 16:56:16 +0900

    1120 modal transition

--- vue/vueshop/src/App.vue
+++ vue/vueshop/src/App.vue
@@ -11,11 +11,27 @@
     <a v-for="item in navList" :key="item">{{ item }}</a>
   </div>
 
+  <!-- 모달 -->
+  <Transition name="modalAni">
+    <div class="bg_black" v-if="modalFlg" >
+      <div class="bg_white">
+        <img :src="modalProduct.img" alt="img">
+        <h4>{{ modalProduct.name }}</h4>
+        <p>{{ modalProduct.content }}</p>
+        <p>{{ modalProduct.price }} 원</p>
+        <p>신고수 : {{ modalProduct.reportCnt }}</p>
+        <button @click="modalFlg = false">닫기</button>
+      </div>
+    </div>
+  </Transition>
+
   <!-- 상품 리스트-->
   <div>
-    <div v-for="item in products" :key="item">
-      <h4>{{ item.name }}</h4>
+    <div v-for="(item, i) in products" :key="i">
+      <h4 @click="modalOpen(item)">{{ item.name }}</h4>
       <p>{{ item.price }} 원</p>
+      <button @click="plusOne(i)">허위 매물 신고</button>
+      <span>신고수 : {{ item.reportCnt }}</span>
     </div>
   </div>
   <!-- <div>
@@ -35,6 +51,8 @@
 </template>
 
 <script>
+import data from './assets/js/data.js';
+
 export default {
   name: 'App',
 
@@ -44,19 +62,30 @@ export default {
       navList: ['홈', '상품', '기타', '문의'],
       sty_color_red: 'color: blue',
       // products : ['양말', '티셔츠', '바지'], 
-      // prices : [1500, 24000, 50000],   
-      products: [
-        {'name': '양말', 'price': 1500},
-        {'name': '티셔츠', 'price': 24000},
-        {'name': '바지', 'price': 50000}
-      ]
+      // prices : [1500, 24000, 50000],  
+      // reportCnt: 0, 
+      products: data,
+      modalFlg: false,
+      modalProduct: {},
     }
   },
 
+  // methods : 함수를 정의하는 영역
+  methods: {
+    plusOne(i) {
+      this.products[i].reportCnt++;
+    },
+    modalOpen(item) {
+      this.modalFlg = true;
+      this.modalProduct = item;
+    },
+  },
 }
 </script>
 
 <style>
+@import url('./assets/css/common.css');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -66,6 +95,7 @@ export default {
   margin-top: 60px;
 }
 
+/* css 파일로 이관
 .nav {
   background-color: #2c3e50;
   padding: 15px;
@@ -75,5 +105,5 @@ export default {
   color: #fff;
   padding: 10px;
   text-decoration: none;
-}
+} */
 </style>
---
+++ vue/vueshop/src/assets/js/data.js
@@ -0,0 +1,5 @@
+export default [
+	{name: '양말', content: '매우 아름다운 양말입니다.', price: 1500, reportCnt: 0, img: require('@/assets/img/00.jpg')},
+	{name: '티셔츠', content: '매우 아름다운 티셔츠입니다.', price: 24000, reportCnt: 0, img: require('@/assets/img/01.jpg')},
+	{name: '바지', content: '매우 아름다운 바지입니다.', price: 50000, reportCnt: 0, img: require('@/assets/img/02.jpg')}
+]
